b26b0c572822b59a5fca76844a8ee6dc7dafa0f1
aljoh <reachingalfred@gmail.com>
2017-11-11 22:09:01 +0100

    datatable stored as objects in array

--- src/cooper.js
+++ src/cooper.js
@@ -1,26 +1,38 @@
 objects = [
-  {gender: "man", minAge: 13, maxAge: 14, aboveAverage: 2699, average: 2399, belowAverage: 2199},
-  {gender: "man", minAge: 15, maxAge: 16, aboveAverage: 2799, average: 2499, belowAverage: 2299}
+  {gender: "man", minAge: 13, maxAge: 14, aboveAverage: 2699, average: 2399, belowAverage: 2199, poor: 2100},
+  {gender: "man", minAge: 15, maxAge: 16, aboveAverage: 2799, average: 2499, belowAverage: 2299, poor: 2200},
+  {gender: "man", minAge: 17, maxAge: 19, aboveAverage: 2999, average: 2699, belowAverage: 2499, poor: 2300},
+  {gender: "man", minAge: 20, maxAge: 29, aboveAverage: 2799, average: 2399, belowAverage: 2199, poor: 1600},
+  {gender: "man", minAge: 30, maxAge: 39, aboveAverage: 2699, average: 2299, belowAverage: 1999, poor: 1500},
+  {gender: "man", minAge: 40, maxAge: 49, aboveAverage: 2499, average: 2099, belowAverage: 1699, poor: 1400},
+  {gender: "man", minAge: 50, maxAge: 99, aboveAverage: 2399, average: 1999, belowAverage: 1599, poor: 1300},
+  {gender: "woman", minAge: 13, maxAge: 14, aboveAverage: 1999, average: 1899, belowAverage: 1599, poor: 1500},
+  {gender: "woman", minAge: 15, maxAge: 16, aboveAverage: 2099, average: 1999, belowAverage: 1699, poor: 1600},
+  {gender: "woman", minAge: 17, maxAge: 19, aboveAverage: 2299, average: 2099, belowAverage: 1799, poor: 1700},
+  {gender: "woman", minAge: 20, maxAge: 29, aboveAverage: 2699, average: 2199, belowAverage: 1799, poor: 1500},
+  {gender: "woman", minAge: 30, maxAge: 39, aboveAverage: 2499, average: 1999, belowAverage: 1699, poor: 1400},
+  {gender: "woman", minAge: 40, maxAge: 49, aboveAverage: 2299, average: 1899, belowAverage: 1499, poor: 1200},
+  {gender: "woman", minAge: 50, maxAge: 99, aboveAverage: 2199, average: 1699, belowAverage: 1399, poor: 1100}
 ];
 
 function evaluate(attr){
   this.age = attr.age;
   this.gender = attr.gender;
   this.distance = attr.distance;
-  var object = objects.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
-  return findAssessment(object, this.distance);
+  var ageGroup = objects.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
+  return findAssessment(ageGroup, this.distance);
 }
 
-function findAssessment(obj, distance) {
+var findAssessment = function(obj, distance) {
   if (distance > obj.aboveAverage) {
     return "Excellent";
   } else if (distance > obj.average) {
     return "Above Average";
   } else if (distance > obj.belowAverage) {
     return "Average";
-  } else if (distance > obj.poor) {
+  } else if (distance >= obj.poor) {
     return "Below Average";
   } else if (distance < obj.poor) {
     return "Poor";
   }
-}
+};
