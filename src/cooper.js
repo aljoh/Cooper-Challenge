0d85addaa15607f0f3fbd8f1a44c03a8f5a5689c
aljoh <reachingalfred@gmail.com>
2017-11-11 22:18:07 +0100

    added tests

--- src/cooper.js
+++ src/cooper.js
@@ -1,11 +1,13 @@
-objects = [
+men = [
   {gender: "man", minAge: 13, maxAge: 14, aboveAverage: 2699, average: 2399, belowAverage: 2199, poor: 2100},
   {gender: "man", minAge: 15, maxAge: 16, aboveAverage: 2799, average: 2499, belowAverage: 2299, poor: 2200},
   {gender: "man", minAge: 17, maxAge: 19, aboveAverage: 2999, average: 2699, belowAverage: 2499, poor: 2300},
   {gender: "man", minAge: 20, maxAge: 29, aboveAverage: 2799, average: 2399, belowAverage: 2199, poor: 1600},
   {gender: "man", minAge: 30, maxAge: 39, aboveAverage: 2699, average: 2299, belowAverage: 1999, poor: 1500},
   {gender: "man", minAge: 40, maxAge: 49, aboveAverage: 2499, average: 2099, belowAverage: 1699, poor: 1400},
-  {gender: "man", minAge: 50, maxAge: 99, aboveAverage: 2399, average: 1999, belowAverage: 1599, poor: 1300},
+  {gender: "man", minAge: 50, maxAge: 99, aboveAverage: 2399, average: 1999, belowAverage: 1599, poor: 1300}
+];
+women = [
   {gender: "woman", minAge: 13, maxAge: 14, aboveAverage: 1999, average: 1899, belowAverage: 1599, poor: 1500},
   {gender: "woman", minAge: 15, maxAge: 16, aboveAverage: 2099, average: 1999, belowAverage: 1699, poor: 1600},
   {gender: "woman", minAge: 17, maxAge: 19, aboveAverage: 2299, average: 2099, belowAverage: 1799, poor: 1700},
@@ -19,7 +21,11 @@ function evaluate(attr){
   this.age = attr.age;
   this.gender = attr.gender;
   this.distance = attr.distance;
-  var ageGroup = objects.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
+  if(this.gender == "man") {
+    var ageGroup = men.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
+  } else if (this.gender == "woman") {
+    var ageGroup = women.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
+  }
   return findAssessment(ageGroup, this.distance);
 }
 
