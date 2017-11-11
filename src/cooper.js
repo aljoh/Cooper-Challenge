d1af22458f3c9f80e6f4da468e2714ff532e52d0
aljoh <reachingalfred@gmail.com>
2017-11-11 21:35:07 +0100

    working implementation with objects

--- src/cooper.js
+++ src/cooper.js
@@ -1,15 +1,26 @@
-function cooper(attr){
-  objects = [
-    {gender: "man", minAge: 13, maxAge: 14, aboveAverage: [2400, 2699]}
-    {gender: "man", minAge: 15, maxAge: 17, aboveAverage: [2500, 2799]}
-  ];
-  function findGender(gender) {
-    return function(assessment) {
-      return
-    }
-  }
+objects = [
+  {gender: "man", minAge: 13, maxAge: 14, aboveAverage: 2699, average: 2399, belowAverage: 2199},
+  {gender: "man", minAge: 15, maxAge: 16, aboveAverage: 2799, average: 2499, belowAverage: 2299}
+];
 
-  men.find(findGender('man'))
+function evaluate(attr){
+  this.age = attr.age;
+  this.gender = attr.gender;
+  this.distance = attr.distance;
+  var object = objects.find(obj => this.age >= obj.minAge && this.age <= obj.maxAge);
+  return findAssessment(object, this.distance);
 }
 
-  men.find(findAge('age'))
+function findAssessment(obj, distance) {
+  if (distance > obj.aboveAverage) {
+    return "Excellent";
+  } else if (distance > obj.average) {
+    return "Above Average";
+  } else if (distance > obj.belowAverage) {
+    return "Average";
+  } else if (distance > obj.poor) {
+    return "Below Average";
+  } else if (distance < obj.poor) {
+    return "Poor";
+  }
+}
